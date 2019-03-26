ed80222fe43d0259717fbac26a458bc025b09c7b
Juan Pablo Solano <juanpa_solano@hotmail.com>
2019-03-26 16:27:37 -0500

    updates

--- src/lambda/scrap.js
+++ src/lambda/scrap.js
@@ -1,76 +1,10 @@
-import fetch from "node-fetch";
-import $ from "cheerio";
-import GoogleSpreadsheet from "google-spreadsheet";
-
-var creds = {
-  client_email: process.env.GCS_EMAIL,
-  private_key: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, "\n")
-};
-
-const sheetId = "1E--ADFQ16rbHPfF_wzOj_1IelS9DCmbce3_Xmvzcgkw";
-var doc = new GoogleSpreadsheet(sheetId);
-
-const url = "https://www.asxenergy.com.au/";
+import { fetchAndStore } from "../util/fetch-doc";
 
 exports.handler = async (event, context) => {
-  return fetch(url)
-    .then(res => res.text())
-    .then(html => {
-      const table = $("#home-prices", html);
-      const headers = [];
-      const content = [];
-      table.find("thead td").each((i, el) => {
-        const text = $(el)
-          .text()
-          .trim();
-        headers.push(text === "" ? "year" : text);
-      });
-      table.find("tbody tr").each((i, tr) => {
-        const row = {};
-        $(tr)
-          .find("td")
-          .each((indx, td) => {
-            row[headers[indx]] = $(td).text();
-          });
-        content.push(row);
-      });
-
-      //Ads to google sheets
-
-      doc.useServiceAccountAuth(creds, function(err) {
-        console.log(err);
-        doc.getInfo(function(err, info) {
-          var sheet = info.worksheets[0];
-          console.log(sheet);
-          console.log(content);
-          content.forEach((row, rowIdx) => {
-            console.log({
-              date: new Date().toDateString(),
-              ...row
-            });
-            sheet.addRow(
-              {
-                date: new Date().toDateString(),
-                ...row
-              },
-              function(err, res) {
-                console.log(err);
-                console.log(res);
-                console.log("------------------");
-                if (rowIdx === content.length - 1) {
-                  return {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                      headers,
-                      content
-                    })
-                  };
-                }
-              }
-            );
-          });
-        });
-      });
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+  try {
+    const res = await fetchAndStore();
+    return { statusCode: 200, body: JSON.stringify(res) };
+  } catch (err) {
+    return { statusCode: 422, body: String(error) };
+  }
 };
--- src/util/fetch-doc.js
+++ src/util/fetch-doc.js
@@ -1,42 +1,79 @@
 const fetch = require("node-fetch");
 const $ = require("cheerio");
+var GoogleSpreadsheet = require("google-spreadsheet");
 
 const url = "https://www.asxenergy.com.au/";
-
-const fetchDoc = async (event, context) => {
-  return fetch(url)
-    .then(res => res.text())
-    .then(html => {
-      const table = $("#home-prices", html);
-      const headers = [];
-      const content = [];
-      table.find("thead td").each((i, el) => {
-        const text = $(el)
-          .text()
-          .trim();
-        headers.push(text === "" ? "year" : text);
-      });
-      table.find("tbody tr").each((i, tr) => {
-        const row = {};
-        $(tr)
-          .find("td")
-          .each((indx, td) => {
-            console.log(indx);
-            console.log(headers[indx]);
-            console.log($(td).text());
-            row[headers[indx]] = $(td).text();
-          });
-        content.push(row);
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            headers,
-            content
-          })
-        };
-      });
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+var creds = {
+  client_email: process.env.GCS_EMAIL,
+  private_key: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, "\n")
 };
 
-fetchDoc();
+const sheetId = "1E--ADFQ16rbHPfF_wzOj_1IelS9DCmbce3_Xmvzcgkw";
+var doc = new GoogleSpreadsheet(sheetId);
+
+const storeRow = async (row, sheet) => {
+  return new Promise((resolve, reject) => {
+    sheet.addRow(
+      {
+        date: new Date().toDateString(),
+        ...row
+      },
+      function(err, res) {
+        if (err) return reject(err);
+        return resolve(res);
+      }
+    );
+  });
+};
+
+const initDoc = async content => {
+  return new Promise((resolve, reject) => {
+    doc.useServiceAccountAuth(creds, function(err) {
+      if (err) return reject(err);
+      doc.getInfo(function(err, info) {
+        if (err) return reject(err);
+        var sheet = info.worksheets[0];
+        var promises = content.map(row => storeRow(row, sheet));
+        Promise.all(promises).then(values => {
+          return resolve(values)
+        })
+      });
+    });
+  });
+};
+
+const fetchDoc = async (event, context) => {
+  try {
+    const res = await fetch(url);
+    const html = await res.text();
+
+    const table = $("#home-prices", html);
+    const headers = [];
+    const content = [];
+    table.find("thead td").each((i, el) => {
+      const text = $(el)
+        .text()
+        .trim();
+      headers.push(text === "" ? "year" : text);
+    });
+    table.find("tbody tr").each((i, tr) => {
+      const row = {};
+      $(tr)
+        .find("td")
+        .each((indx, td) => {
+          row[headers[indx]] = $(td).text();
+        });
+      content.push(row);
+    });
+    return content;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const fetchAndStore = async () => {
+  const content = await fetchDoc();
+  const res = await initDoc(content);
+  return res;
+};
+
